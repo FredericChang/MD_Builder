12adbd117bbce6b4dc10871061bca2648ff9fb4a
Frederic <paulpoiu123@gmail.com>
2020-10-11 03:46:33 +0800

    finished the hotkey

--- cloud-doc/src/App.js
+++ cloud-doc/src/App.js
@@ -7,14 +7,14 @@ function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
-          <div className= "col-3 bg-danger left-panel">
+          <div className= "col bg-danger left-panel">
             <FileSearch 
               title='CloudFile'
-              onFileSearch={() => {}}
+              onFileSearch={(value) => { console.log(value)}}
             />
           </div>
-          <div className= "col-9 bg-primary right-panel">
-            <h1> this is right panel</h1>
+          <div className= "col bg-primary right-panel">
+            <h1> right</h1>
           </div>
       </div>
     </div>
--- cloud-doc/src/components/FileSearch.js
+++ cloud-doc/src/components/FileSearch.js
@@ -1,8 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const FileSearch = ({ title, onFileSearch }) => {
     const [ inputActive, setInputActive ] = useState(false)
     const [ value, setValue ] = useState('')
+    let number = useRef(1)
+    number.current++
+    console.log(number.current) //number current 會變化 number則不會變化
+    let node = useRef(null)
+
+    const closeSearch = (e) =>{
+        e.preventDefault()
+        setInputActive(false)
+        setValue('') 
+    }
+    useEffect(() => {
+        const handleInputEvent = (event) =>{
+            const { keyCode } = event
+            if (keyCode === 13 && inputActive ){
+                onFileSearch(value)
+            }else if (keyCode === 27 && inputActive ){
+                closeSearch(event)
+            }
+        }
+        document.addEventListener('keyup', handleInputEvent)
+        return () =>{
+            document.removeEventListener('keyup', handleInputEvent)
+        }
+    })
+    useEffect(() => { //用途用於搜索點亮
+        if (inputActive){
+            node.current.focus()
+        }
+    }, [inputActive])
+
     return(
         <div class="alert alert-primary" role="alert">
             {!inputActive &&
@@ -22,12 +52,13 @@ const FileSearch = ({ title, onFileSearch }) => {
                     <input
                         className="form-control col-8"
                         value={value}
+                        ref = {node}
                         onChange={(e) => {setValue(e.target.value)}}
                     />
                     <button
                         type="button"
                         className="btn btn-primary col-4"
-                        onClick ={ () => { setInputActive(false)}}
+                        onClick ={closeSearch}
                     >
                         Close
                     </button>
