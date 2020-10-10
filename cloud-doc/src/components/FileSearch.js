5267d5aa652a80e4aa84d0cc80fae542416e93b3
Frederic <paulpoiu123@gmail.com>
2020-10-11 01:22:29 +0800

    updated file serach

---
+++ cloud-doc/src/components/FileSearch.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const FileSearch = ({ title, onFileSearch }) => {
+    const [ inputActive, setInputActive ] = useState(false)
+    const [ value, setValue ] = useState('')
+    return(
+        <div class="alert alert-primary" role="alert">
+            {!inputActive &&
+                <div className="d-flex justify-content-between align-items-center"> 
+                    <span>{title}</span> 
+                    <button
+                        type="button"
+                        className="btn btn-primary"
+                        onClick ={ () => { setInputActive(true)}}
+                    >
+                        Search
+                    </button>
+                </div>
+            }
+            {inputActive &&
+                <div className="row"> 
+                    <input
+                        className="form-control col-8"
+                        value={value}
+                        onChange={(e) => {setValue(e.target.value)}}
+                    />
+                    <button
+                        type="button"
+                        className="btn btn-primary col-4"
+                        onClick ={ () => { setInputActive(false)}}
+                    >
+                        Close
+                    </button>
+                </div>
+            }
+        </div>
+    )
+}
+export default FileSearch
