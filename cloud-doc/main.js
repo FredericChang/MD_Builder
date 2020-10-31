312f297388da1cfd3435d337126574dd4f7b040a
Frederic <paulpoiu123@gmail.com>
2020-11-01 03:39:49 +0800

    Added setting

--- cloud-doc/main.js
+++ cloud-doc/main.js
@@ -1,19 +1,35 @@
-const { app, remote, BrowserWindow, Menu } = require('electron') 
+const { app, remote, BrowserWindow, Menu, ipcMain} = require('electron') 
 const isDev = require('electron-is-dev')
+const path = require('path')
 const menuTemplate = require('./src/menuTemplate')
-let mainWindow;
+const AppWindow = require('./src/AppWindow')
+let mainWindow, settingsWindow
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
+    const mainWindowConfig = {
         width: 1024,
         height: 680,
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true,
-        }
-    })
+    }
+
     const urlLocation = isDev ? 'http://localhost:3000' : 'dummyurl'
-    mainWindow.loadURL(urlLocation)
+    mainWindow = new AppWindow(mainWindowConfig, urlLocation)
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+    // mainWindow.loadURL(urlLocation) bypass with mainWindow
+    ipcMain.on('open-settings-window', () => {
+        const settingsWindowConfig = {
+            width: 500,
+            height: 400,
+            parent: mainWindow
+        }    
+        const settingsFileLocation = `file://${path.join(__dirname, './settings/settings.html')}`
+        settingsWindow = new AppWindow(settingsWindowConfig, settingsFileLocation)
+        settingsWindow.removeMenu()
+        settingsWindow.on('closed', () => {
+          settingsWindow = null
+        })
+    })
     const menu = Menu.buildFromTemplate(menuTemplate)
     Menu.setApplicationMenu(menu)
 })
